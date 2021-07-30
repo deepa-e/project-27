3b71377c7966a22c8f75d33749e144fe403d084b
FlowerSeerat <seerat3208@gmail.com>
2021-07-30 08:41:47 -0400

    changes done

--- sketch.js
+++ sketch.js
@@ -71,13 +71,13 @@ function draw() {
 // }
 
 // function keyPressed() {
-// 	if (keyCode === UP_ARROW) {
+ //	if (keyCode === UP_ARROW) {
 // 		Matter.Body.applyForce(bob1.body,{x:-50,y:-45});
-// 	}
-// }
+ //	}
+ //}
 
-// function keyPressed() {
-// 	if (keyCode === UP_ARROW) {
-// 		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45});
-// 	}
-// }
+ function keyPressed() {
+ 	if (keyCode === UP_ARROW) {
+ 		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45});
+ 	}
+ }
